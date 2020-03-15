45e04b50f86cdb7afc081a51cce65acb1c7e21e9
raypan891108 <raypan891108@gmail.com>
2020-03-15 18:56:08 +0800

    MainShape

--- MainShape.java
+++ MainShape.java
@@ -4,19 +4,9 @@ public class MainShape{
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         ArrayList <Shape> ShapeList = new ArrayList <Shape>();
-        // float r;
-        // float length;
-        // float width;
-        // float a;
-        // float b;
-        // int angle;
-        // int operation;
         String n;
         while(true){
-            // Shape.menu();
-            // operation = scn.nextInt();
             n = scn.nextLine();
-            String tmp = "show";
             if(n.equals("-1")){
                 break;
             }else if(n.equals("show")){
